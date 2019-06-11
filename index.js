f92db07aa238b85f402c8f45da4711f7d1732d6e
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-11 14:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  let newA = kittens.slice(0,kittens.length)
+  let newA = kittens.slice(0,kittens.length - 1)
   return newA
 }
 
